bba5e0078fe81374c06e76713cae76b6cdf5e73c
vmoehl <verena.moehle@yahoo.de>
2021-11-13 20:32:36 +0100

    try JS

--- src/main/java/de/ostfalia/bips/ws21/start/data/CheckProjektTeam.java
+++ src/main/java/de/ostfalia/bips/ws21/start/data/CheckProjektTeam.java
@@ -20,11 +20,31 @@ public class CheckProjektTeam implements JavaDelegate {
     @Override
     
     public void execute(DelegateExecution execution) throws Exception {
-    	final List<String> status = new ArrayList<String>();
+    	final Connection connection = DatabaseConnection.getConnection();
+        final PreparedStatement statement = connection.prepareStatement("SELECT m.idM, m.name FROM mitarbeiter m Join projekte_has_mitarbeiter pm On m.idM = pm.idM "
+        		+ "Join (SELECT idP from projekte_has_mitarbeiter Where idM = ?) p "
+        		+ "On pm.idP = p.idP Where m.idM != ? Group By m.idM Order By m.idM");
+        statement.setInt(1, (int) execution.getVariable("MITARBEITER_ID"));
+        statement.setInt(2, (int) execution.getVariable("MITARBEITER_ID"));
+        final ResultSet resultSet = statement.executeQuery();
+        final Map<Integer, String> employees = new HashMap<>();
+        while(resultSet.next()) {
+        	employees.put(resultSet.getInt("idM"), resultSet.getString("name"));
+        }
+        statement.close();
+        connection.close();
+        execution.setVariable("PROJEKTTEAM", Variables.objectValue(employees)
+                .serializationDataFormat(Variables.SerializationDataFormats.JSON)
+                .create());
+        execution.setVariable("PROJEKTTEAM_GROESSE", employees.size());
+    	
+        final List<String> status = new ArrayList<String>();
     	status.add("genehmigt");
     	status.add("abgelehnt");
         execution.setVariable("AVAILABLE_STATUS", Variables.objectValue(status)
                 .serializationDataFormat(Variables.SerializationDataFormats.JSON)
                 .create());
+       
+        
     }
 }
